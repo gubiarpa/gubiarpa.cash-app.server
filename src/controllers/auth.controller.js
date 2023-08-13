762d8f6d23bff37e51f7a095f9cc42559184a1fc
Billy Arredondo <billy.arredondo@pucp.pe>
2023-08-13 18:44:20 -0500

    Logout endpoint

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -82,3 +82,12 @@ export const login = async (req, res) => {
 		return res.status(500).json({ message: error.message });
 	}
 };
+
+export const logout = async (req, res) => {
+	res.cookie("token", "", {
+		httpOnly: true,
+		secure: true,
+		expires: new Date(0)
+	});
+	return res.sendStatus(200);
+};
