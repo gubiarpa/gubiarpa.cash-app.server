a477fa90bf3d9c090e037af66300064b277fde25
Billy Arredondo <billy.arredondo@pucp.pe>
2023-08-13 13:04:06 -0500

    Register and Login endpoints

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from "express";
 import morgan from "morgan";
 
+import authRoutes from "./routes/auth.route.js";
+
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 export default app;
--- src/config.js
+++ src/config.js
@@ -5,3 +5,4 @@ dotenv.config();
 export const PORT = process.env.PORT || 4000;
 export const MONGODB_URI =
 	process.env.MONGODB_URI || "mongodb://localhost/cash-app";
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "S3CR3T";
