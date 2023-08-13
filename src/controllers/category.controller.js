bca4677c2f703266168903c5ea7f611b880f5e7f
Billy Arredondo <billy.arredondo@pucp.pe>
2023-08-13 15:48:14 -0500

    Category CRUD methods

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,14 +1,61 @@
 import Category from "../models/category.model.js";
 
-export const getAll = async (req, res) => {
+export const getAllCategories = async (_, res) => {
 	try {
 		const categories = await Category.find();
-
-		if (!categories)
-			return res.status(400).json({ message: "Category not found" });
-
 		return res.json(categories);
 	} catch (err) {
 		return res.status(500).json({ message: error.message });
+	}
+};
+
+export const getCategory = async (req, res) => {
+	try {
+		const category = await Category.findById(req.params.id);
+		if (!category)
+			return res.status(404).json({ message: "Category not found" });
+		return res.json(category);
+	} catch (err) {
+		return res.status(500).json({ message: err.message });
+	}
+};
+
+export const createCategory = async (req, res) => {
+	try {
+		const { description } = req.body;
+		const createdCategory = new Category({
+			description
+		});
+		await createdCategory.save();
+		res.status(201).json(createdCategory);
+	} catch (err) {
+		return res.status(500).json({ message: err.message });
+	}
+};
+
+export const updateCategory = async (req, res) => {
+	try {
+		const { description } = req.body;
+		const updatedCategory = await Category.findByIdAndUpdate(
+			{ _id: req.params.id },
+			{ description },
+			{ new: true }
+		);
+		if (!updatedCategory)
+			return res.status(404).json({ message: "Category not found" });
+		return res.json(updatedCategory);
+	} catch (err) {
+		return res.status(500).json({ message: err.message });
+	}
+};
+
+export const deleteCategory = async (req, res) => {
+	try {
+		const deletedCategory = await Category.findByIdAndDelete(req.params.id);
+		if (!deletedCategory)
+			return res.status(404).json({ message: "Category not found" });
+		return res.sendStatus(204);
+	} catch (err) {
+		return res.status(500).json({ message: err.message });
 	}
 };
--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
-import { getAll } from "../controllers/category.controller.js";
+import {
+	createCategory,
+	deleteCategory,
+	getAllCategories,
+	getCategory,
+	updateCategory
+} from "../controllers/category.controller.js";
 
 const router = Router();
 
-router.get("/", getAll);
+router.get("/", getAllCategories);
+router.get("/:id", getCategory);
+router.post("/", createCategory);
+router.put("/:id", updateCategory);
+router.delete("/:id", deleteCategory);
 
 export default router;
